4fd6bc4261b2f16dde5e4f2102ddeea50088ca2f
chairman's chair <likwang@ethz.ch>
2025-10-02 23:00:09 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_STACK_PROJECT_ID: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
-    NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY: process.env.NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY,
-  },
 }
 
 export default nextConfig
